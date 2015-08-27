cb2c1cd6ef6490d7f496aa5931bb21d4e595c0d3
kuakling <kuakling@gmail.com>
2015-08-27 11:50:45 +0700

    Update CKEditorAsset.php

--- CKEditorAsset.php
+++ CKEditorAsset.php
@@ -11,7 +11,7 @@ use yii\web\AssetBundle;
  */
 class CKEditorAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/ckeditor';
+    public $sourcePath = '@vendor/ckeditor/ckeditor';
     public $css = [
     ];
     public $js = [
