3854d78a9c04d5bce6541853bc946188c92b6db2
kuakling <kuakling@gmail.com>
2015-08-27 10:28:23 +0700

    Update CKEditor.php

--- CKEditor.php
+++ CKEditor.php
@@ -3,6 +3,7 @@
 namespace kuakling\keditor;
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Json;
 use yii\widgets\InputWidget;
 class CKEditor extends InputWidget
@@ -11,6 +12,7 @@ class CKEditor extends InputWidget
     /**
      * @inheritdoc
      */
+    public $filemanager = false;
     public function init()
     {
         parent::init();
@@ -38,14 +40,43 @@ class CKEditor extends InputWidget
         $view = $this->getView();
         CKEditorAsset::register($view);
         $id = $this->options['id'];
+
+        if ($this->filemanager) {
+            $kcfinder = KCFinderAsset::register($view);
+            $browse = [
+                'filebrowserBrowseUrl' => $kcfinder->baseUrl.'/browse.php?opener=ckeditor&type=files',
+                'filebrowserImageBrowseUrl' => $kcfinder->baseUrl.'/browse.php?opener=ckeditor&type=images',
+                'filebrowserFlashBrowseUrl' => $kcfinder->baseUrl.'/browse.php?opener=ckeditor&type=flash',
+                'filebrowserUploadUrl' => $kcfinder->baseUrl.'/upload.php?opener=ckeditor&type=files',
+                'filebrowserImageUploadUrl' => $kcfinder->baseUrl.'/upload.php?opener=ckeditor&type=images',
+                'filebrowserFlashUploadUrl' => $kcfinder->baseUrl.'/upload.php?opener=ckeditor&type=flash',
+            ];
+            $this->clientOptions = ArrayHelper::merge($this->clientOptions, $browse);
+            $kcfOptions = [
+                'disabled' => false,
+                'uploadURL' => \Yii::getAlias('@web').'/upload',
+                'access' => [
+                    'files' => [
+                        'upload' => true,
+                        'delete' => false,
+                        'copy' => false,
+                        'move' => false,
+                        'rename' => false,
+                    ],
+                    'dirs' => [
+                        'create' => true,
+                        'delete' => false,
+                        'rename' => false,
+                    ],
+                ],
+            ];
+            \Yii::$app->session->set('KCFINDER', $kcfOptions);
+        }
+
         $options = $this->clientOptions !== false && !empty($this->clientOptions)
             ? Json::encode($this->clientOptions)
             : '{}';
         $js[] = "CKEDITOR.replace('$id', $options);";
-        /*$js[] = "dosamigos.ckEditorWidget.registerOnChangeHandler('$id');";
-        if (isset($this->clientOptions['filebrowserUploadUrl'])) {
-            $js[] = "dosamigos.ckEditorWidget.registerCsrfImageUploadHandler();";
-        }*/
         $view->registerJs(implode("\n", $js));
     }
 }
