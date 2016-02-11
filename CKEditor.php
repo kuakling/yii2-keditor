9399dd0ec5b0bc537d67fc1d327ffc613ce7ca38
kuakling <kuakling@gmail.com>
2016-02-11 18:12:22 +0700

    Update CKEditor.php
    copy upload.htaccess to kcfinder asset

--- CKEditor.php
+++ CKEditor.php
@@ -7,6 +7,7 @@ use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Json;
 use yii\widgets\InputWidget;
+
 class CKEditor extends InputWidget
 {
     use CKEditorTrait;
@@ -71,6 +72,11 @@ class CKEditor extends InputWidget
                 'uploadURL' => $this->uploadURL,
             ];
             Yii::$app->session->set('KCFINDER', $kcfOptions);
+
+
+            $htSource = __DIR__.'/upload.htaccess';
+            $htDest = $kcfinder->basePath.'/conf/upload.htaccess';
+            copy($htSource, $htDest);
         }
 
         $options = $this->clientOptions !== false && !empty($this->clientOptions)
